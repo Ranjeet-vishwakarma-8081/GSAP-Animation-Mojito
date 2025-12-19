08db0d6dcafb9fb7050a59df381e68f3485af612
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-12-19 18:50:28 +0530

    Implemented the Hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
       },
       {
         backgroundColor: "#00000050",
-        filter: "blur(10px)",
+        backgroundFilter: "blur(10px)",
         duration: 1,
         ease: "power1.inOut",
       }
@@ -26,7 +26,7 @@ const Navbar = () => {
   return (
     <nav>
       <div>
-        <a href="#home" className="flex items-center gap-2">
+        <a href="#hero" className="flex items-center gap-2">
           <img src="/images/logo.png" alt="logo" />
           <p>Velvet Pour</p> {/* Company Name */}
         </a>
